94f1ab405b7a476e5bdf63a63f5ab10c18f2e442
LoraBorisova <76106295+LoraBorisova@users.noreply.github.com>
2021-08-16 15:07:54 +0300

    first commit

---
+++ thirdSlideText.js
@@ -0,0 +1,20 @@
+var textWrapper = document.querySelector('.ml15');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml15 .letter',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 1200,
+    delay: (el, i) => 500 + 30 * i
+  }).add({
+    targets: '.ml15 .letter',
+    translateX: [0,-30],
+    opacity: [1,0],
+    easing: "easeInExpo",
+    duration: 1200,
+    delay: (el, i) => 100 + 30 * i
+  });
